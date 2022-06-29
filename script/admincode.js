7d57e731b925a2cf1ab2fb989fa2422db16f3480
MuddathirDawood <dmuddathir@gmail.com>
2022-06-29 16:31:12 +0200

    admin pt3

--- script/admincode.js
+++ script/admincode.js
@@ -1,5 +1,41 @@
-let lands = JSON.parse(localStorage.getItem('land'));
+let properties = JSON.parse(localStorage.getItem('land'));
 
-document.querySelector('#total').innerHTML = lands.length;
+document.querySelector('#total').innerHTML = properties.length;
 
-console.table(lands);
+console.table(properties);
+
+function loadData(prop){
+    prop.forEach((land)=>{
+    let rand = land.price * 15.93
+    document.querySelector('#properties').innerHTML += `
+            <div>
+                <div class="row">
+                    <div class="col-1">
+                        <p>${land.id}</p>
+                    </div>
+                    <div class="col-4 text-start">
+                    <p>${land.address.region}/${land.address.neighbourhood}/${land.address.street}</p>
+                    </div>
+                    <div class="col-2">
+                    <p>$${land.price}/ R${rand}</p>
+                    </div>
+                    <div class="col-1">
+                    <p>${land.rooms.bedrooms}</p>
+                    </div>
+                    <div class="col-1">
+                    <p>${land.rooms.bathrooms}</p>
+                    </div>
+                    <div class="col-1">
+                    <p>${land.size}</p>
+                    </div>
+                    <div class="col-2">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-solid fa-trash"></i>
+                    </div>
+                </div>
+            </div>
+    `
+});
+}
+
+loadData(properties);
